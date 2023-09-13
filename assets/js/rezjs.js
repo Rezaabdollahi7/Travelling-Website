201d1f988fca72f729592999ab10abf4a3a00b8d
Sreza <130074829+Rezaabdollahi7@users.noreply.github.com>
2023-09-13 12:18:40 +0330

    add bootstrap rtl

--- assets/js/rezjs.js
+++
@@ -1,9 +0,0 @@
-// add active class to accordion-header when click on accordionButtons (for change border-radius)
-const accordionButtons = document.querySelectorAll('.accordion-button');
-const accordionHeaders = document.querySelectorAll('.accordion-header');
-
-accordionButtons.forEach((button, index) => {
-    button.addEventListener('click', function () {
-        accordionHeaders[index].classList.toggle('active');
-    });
-});
